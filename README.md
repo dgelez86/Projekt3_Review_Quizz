c0169d2382ff3aed2de6f9c6941e6f6bb6184810
dgelez86 <dgarlez86@outlook.com>
2021-03-06 18:32:39 +0100

    added js files

--- README.md
+++ README.md
@@ -1,23 +1,62 @@
-# Projekt3_Quizz_Fetch
+# Projekt3_Reviewed_Quizz
+
+## Main
+
+Logic
 
 
-Input
-    - getNodes
-    - getUserInput
-    - getCorrectAnswers
-    - getDifficulty
+## Screens
+
+### ConfScreen
+
+Properties: 
+- difficulty 
+- type
+- howMany
+
+Methods: 
+- displayConfScreen() -> creates nodes needed
+- getQuestionsFromAPI() -> fetch
+
+### EndScreen
+
+Methods:
+- getQuestionsAnswered() -> retrieve questions answered from Firebase
+- displayResults() -> show all questions, green if is correct, red if is incorrect
 
 
-Process
-    - assessQuizz
-    - generateReport
-    - displayResults
+## Question
+
+### Question
+
+Properties:
+- questions
+
+Methods:
+- pushQuestions() -> save questions in an array
+- displayQuestion() -> creates nodes for question and answers
+- assessQuestion() -> assess if question is correct or incorrect
+- getMoreQuestions() -> display if user wants more questions -> call displayConfScreen()
 
 
-Output
-    - getData -> fetch opentdb.com
-    - createNodes
+## Game
 
+### Player
 
-Initial page
-    - filter questions: difficulty, type....and hao many questions
+Properties:
+- totalTime
+- questionTime
+- score
+
+Methods:
+- getScore(), addScore()
+- addTime() -> add time of current question to total
+
+### Top10
+
+Properties:
+- top10
+
+Methods:
+- makeTop10()
+
--- js/screens.js
+++ js/screens.js
@@ -1,8 +1,122 @@
-class initScreen {
+class confScreen {
+
+    constructor(difficulty, type, howMany) {
+        this.difficulty = difficulty
+        this.type = type
+        this.howMany = howMany
+    }
+
+    static displayConfScreen() {
+
+        confScreen.createNodes().then(confScreen.getUserConf)
+
+    }
+
+    static getQuestionsFromAPI() {
+
+        
+
+    }
+
+    static createNodes() {
+
+        return new Promise(resolve => {
+
+            let wrapper = document.createElement("div")
+                wrapper.classList.add("initWrapper")
+            let title = document.createTextNode("Quizz")
+            wrapper.appendChild(title)
+            document.querySelector("body").appendChild(wrapper)
+
+            let difficulty = document.createElement("select")
+                difficulty.setAttribute("id", "difficulty")
+                let difLabel = document.createElement("label")
+                    difLabel.setAttribute("for", "difficulty")
+                    difficulty.appendChild(difLabel)
+                let dif1 = document.createElement("option")
+                    dif1.setAttribute("value", "easy")
+                    let difText1 = document.createTextNode("easy")
+                    dif1.appendChild(difText1)
+                let dif2 = document.createElement("option")
+                    dif2.setAttribute("value", "medium")
+                    let difText2 = document.createTextNode("medium")
+                    dif2.appendChild(difText2)
+                let dif3 = document.createElement("option")
+                    dif3.setAttribute("value", "hard")
+                    let difText3 = document.createTextNode("hard")
+                    dif3.appendChild(difText3)
+            difficulty.appendChild(difLabel)
+            dif1.appendChild(difText1)
+            dif2.appendChild(difText2)
+            dif3.appendChild(difText3)
+            difficulty.appendChild(dif1)
+            difficulty.appendChild(dif2)
+            difficulty.appendChild(dif3)
+            wrapper.appendChild(difficulty)
+
+            let type = document.createElement("select")
+                type.setAttribute("id", "type")
+                let typeLabel = document.createElement("label")
+                typeLabel.setAttribute("for", "type")   
+                let type1 = document.createElement("option")
+                    type1.setAttribute("value", "anyType")
+                    let typeText1 = document.createTextNode("Any type")   
+                let type2 = document.createElement("option")
+                    type2.setAttribute("value", "multiple")
+                    let typeText2 = document.createTextNode("Multiple choice")
+                let type3 = document.createElement("option")
+                    type3.setAttribute("value", "trueFalse")
+                    let typeText3 = document.createTextNode("True / False")
+            type.appendChild(typeLabel)
+            type1.appendChild(typeText1)
+            type2.appendChild(typeText2)
+            type3.appendChild(typeText3)
+            type.appendChild(type1)
+            type.appendChild(type2)
+            type.appendChild(type3)
+            wrapper.appendChild(type)
+
+            let howMany = document.createElement("input")
+                howMany.setAttribute("type", "number")
+                howMany.setAttribute("id", "howMany")
+                howMany.setAttribute("min", "0")
+                howMany.setAttribute("max", "50")
+                let howLabel = document.createElement("label")
+                    howLabel.setAttribute("for", "howMany")
+                    howMany.appendChild(howLabel)
+            howMany.appendChild(howLabel)
+            wrapper.appendChild(howMany)
+
+            let button = document.createElement("input")
+                button.setAttribute("type", "button")
+                button.setAttribute("value", "agree")
+                button.setAttribute("id", "searchButton")
+                button.addEventListener("click", () => confScreen.getQuestionsFromAPI())
+            wrapper.appendChild(button)
+
+            resolve()
+
+        })
+    
+
+    }
+
+    static getUserConf() {
+
+        console.log("getConf")
+
+        let dif = document.querySelector("#difficulty").value
+        let type = document.querySelector("#difficulty").value
+        let howMany = document.querySelector("#difficulty").value
+        console.log(new confScreen(dif, type, howMany))
+
+    }
 
 }
 
 
 class endScreen {
-    
+
 }
+
+confScreen.displayConfScreen()
